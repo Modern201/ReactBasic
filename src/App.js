5559430e98fd7bb58fa633afe2a43efa81493a4d
Lyo <moderncreation.kr@gmail.com>
2017-03-17 15:52:44 +0900

    react base code

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name : ''
+    };
+  }
+  render() {
+    return (
+      <div>
+          <h1>Hello!!!!!!!!! {this.state.name}</h1>
+          <button onClick={()=>{this.setState({name:
+            'Lyo'});}}>Click Me! </button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: __dirname + '/public',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 7777,
+        contentBase: __dirname + '/public'
+    },
+
+    module:
+    {
+        loaders: [
+            {
+              test: /\.js$/,
+              loader: ['react-hot-loader','babel-loader?' + JSON.stringify({
+                cacheDirectory: true,
+                presets: ['es2015', 'react'],
+              })],
+              exclude: /node_modules/
+
+            }
+        ]
+    }
+};

062ba23463828cdec60362645822487678de21e8
Lyo <moderncreation.kr@gmail.com>
2017-03-17 16:08:18 +0900

    fixed App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
-          <h1>Hello!!!!!!!!! {this.state.name}</h1>
+          <h1>Hello!! {this.state.name}</h1>
           <button onClick={()=>{this.setState({name:
             'Lyo'});}}>Click Me! </button>
       </div>
